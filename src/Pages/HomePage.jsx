5ea480298993dc5ffc436bfb255ebdfc1cd74f77
Emeline <emeline.desa@outlook.fr>
2024-10-10 14:05:42 +0200

    image arrière-plan

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,41 +3,37 @@ import ChampionsServices from "../Services/ChampionsServices";
 import ChampionsCards from "../Components/ChampionsCards";
 import { Container } from "react-bootstrap";
 
-
-
 const HomePage = () => {
-   
-    const[Champions,setChampions] = useState({});
- 
-   
+    const [Champions, setChampions] = useState({});
 
     const fetchChampions = async () => {
         try {
             const response = await ChampionsServices.GetALLChampions();
             setChampions(response.data.data);
-        
             console.log(response.data.data);
-           
         } catch (error) {
-          console.log(error);
+            console.log(error);
         }
-    } 
+    };
 
-    useEffect(() =>
-    {
+    useEffect(() => {
         fetchChampions();
     }, []);
 
-    return <Container className="d-flex flex-column align-items-center">
-    <h1>All Champions</h1>
-    <div className="d-flex justify-content-center flex-wrap gap-5">
-    {Object.entries(Champions).map((Champion) => {
-        return <ChampionsCards  ChampionsCards={Champion[1]} key={Champion[1].id}/>
+    return (
+        <Container className="home-page">
+            <h1>All Champions</h1>
+            <div className="d-flex justify-content-center flex-wrap gap-5">
+                {Object.entries(Champions).map((Champion) => {
+                    return (
+                        <ChampionsCards ChampionsCards={Champion[1]} key={Champion[1].id} />
+                    );
+                })}
+            </div>
+        </Container>
+    );
+};
 
-    })}
-
-</div>
-  </Container>
-}
- 
 export default HomePage;
+
+
